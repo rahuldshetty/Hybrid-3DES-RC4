7f80a2b40ace7a163497242c72fcc7364942cc7d
Rahul D Shetty <35rahuldshetty@gmail.com>
2019-03-27 23:03:05 +0530

    Add files via upload

--- src/DES.py
+++ src/DES.py
@@ -1,5 +1,26 @@
 import math
 
+initPBox = [
+			58,50,42,34,26,18,10,2,
+			60,52,44,36,28,20,12,4,
+			62,54,46,38,30,22,14,6,
+			64,56,48,40,32,24,16,8,
+			57,49,41,33,25,17,9,1,
+			59,51,43,35,27,19,11,3,
+			61,53,45,37,29,21,13,5,
+			63,55,47,39,31,23,15,7
+			]
+
+inversePBox = [
+			40,8,48,16,56,24,64,32,
+			39,7,47,15,55,23,63,31,
+			38,6,46,14,54,22,62,30,
+			37,5,45,13,53,21,61,29,
+			36,4,44,12,52,20,60,28,
+			35,3,43,11,51,19,59,27,
+			34,2,42,10,50,18,58,26,
+			33,1,41,9,49,17,57,25	
+]
 
 class DES:
 
@@ -67,12 +88,51 @@ class DES:
             self.keys.append(finalKey)
         # resultant key added to self.keys list
 
-    def applyDES(self, key, text, type=ENCRYPT, padding=False):
+
+    def permute(self,lists,box):
+    	if len(lists)!=len(box):
+    		raise Exception("Invalid permutation objects")
+    	newList=[0]*len(lists)
+    	for i,pos in enumerate(box):
+    		newList[i]=lists[pos-1]
+    	return newList
+
+
+
+
+    def applyDES(self, key, text, type=ENCRYPT):
         if len(key) < 8:
             raise Exception("Key should be 8 bytes long.")
         else:
             key = self.tobin(key[:8])
             self.genKeys(key)
+
+        if type==self.DECRYPT:
+        	self.keys=self.keys[::-1]
+
+        binText = self.tobin(text)
+
+        blocks=[binText[i:i+64] for i in range(0,len(binText),64)]
+
+        for i in range(len(blocks)):
+        	blocks[i]+=[0]*(64-len(blocks[i]))
+
+        for block in blocks:
+        	#init permutation
+        	initList = self.permute(block,initPBox)
+
+        	data = initList
+
+        	finalList = self.permute(data,inversePBox)
+
+        	
+
+
+      
+
+
+
+
 
 
 if __name__ == "__main__":
