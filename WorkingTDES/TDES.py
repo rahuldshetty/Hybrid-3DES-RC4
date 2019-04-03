4fe4286f696e90b3064c49034f3cf4e08fa79a57
Rahul D Shetty <35rahuldshetty@gmail.com>
2019-04-03 20:37:20 +0530

    Add files via upload

--- WorkingTDES/TDES.py
+++ WorkingTDES/TDES.py
@@ -1,26 +1,28 @@
 from DES import *
 from utility import *
 
-
 class TDES:
-
+    
     @staticmethod
-    def applyTDES(text, keys, mode=ENCRYPT):
-        res = text
-        if mode == DECRYPT:
-            keys = keys[::-1]
+    def applyTDES(text,keys,mode=ENCRYPT):
+        res=text
+        if mode==DECRYPT:keys=keys[::-1]
         for key in keys:
-            if mode == ENCRYPT:
-                res = DES.encrypt(res, key)
+            if mode==ENCRYPT:
+                res = DES.encrypt(res,key)
             else:
-                res = DES.decrypt(res, key)
-            mode ^= 1
+                res=DES.decrypt(res,key)
+            mode^=1
         return res
+                
 
     @staticmethod
-    def encrypt(text, keys):
-        return TDES.applyTDES(text, keys, ENCRYPT)
-
+    def encrypt(text,keys):
+        return TDES.applyTDES(text,keys,ENCRYPT)
+    
     @staticmethod
-    def decrypt(text, keys):
-        return TDES.applyTDES(text, keys, DECRYPT)
+    def decrypt(text,keys):
+        return TDES.applyTDES(text,keys,DECRYPT)
+    
+    
+    
--- WorkingTDES/utility.py
+++ WorkingTDES/utility.py
@@ -1,16 +1,16 @@
-def str2bin(string, fill=0):
-    text = list(map(ord, string))
-    binVal = ""
+def str2bin(string,fill=0):
+    text=list(map(ord,string))
+    binVal=""
     for i in text:
         f = [int(x) for x in "{0:b}".format(i)]
-        f = "".join([str(x) for x in f]).zfill(fill)
-        binVal += f
+        f="".join([str(x) for x in f]).zfill(fill)
+        binVal+=f
     return binVal
-
-
-def bin2str(binString, read=8):
-    blocks = [binString[i:i+read] for i in range(0, len(binString), read)]
-    text = ""
+        
+def bin2str(binString,read=8):
+    blocks=[binString[i:i+read] for i in range(0,len(binString),read)]
+    text=""
     for block in blocks:
-        text += chr(int(block, 2))
+        text+=chr(int(block,2))
     return text
+        
